090f9fd4798ff2206db0e8369e5b1ed3458d5ba5
Brian Meek <brian.meek@williams.com>
2020-08-22 14:31:44 -0500

    Adding coreui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { AppHeaderModule, AppFooterModule, AppAsideModule, AppSidebarModule, AppBreadcrumbModule } from '@coreui/angular';
+const COREUI = [
+  AppHeaderModule,
+  AppFooterModule,
+  AppAsideModule,
+  AppSidebarModule,
+  AppBreadcrumbModule
+];
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+import { TabsModule, TabsetConfig } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ChartsModule } from 'ng2-charts';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: DashboardComponent,
+      children: [{
+        path: '',
+        component: AboutComponent
+      }]
+    }]),
+    BrowserModule,
+    TabsModule.forRoot(),
+    PerfectScrollbarModule,
+    BsDropdownModule.forRoot(),
+    ChartsModule,
+    COREUI
   ],
-  providers: [],
+  providers: [
+    TabsetConfig
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+  title = 'Chronos';
+  public sidebarMinimized = false;
+  public navItems = [];
+
+  toggleMinimize(e): void {
+    this.sidebarMinimized = e;
+  }
+
+}
